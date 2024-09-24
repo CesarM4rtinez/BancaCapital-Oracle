4d7ffffa13b372536952368944e92ed13965f781
César Martínez <119742694+CesarM4rtinez@users.noreply.github.com>
2024-09-24 01:42:08 -0600

    Update ELIMINAR TODAS LAS TABLAS DE UNA CONEXIÓN DE ORACLE XE.sql

--- ELIMINAR TODAS LAS TABLAS DE UNA CONEXIÓN DE ORACLE XE.sql
+++ ELIMINAR TODAS LAS TABLAS DE UNA CONEXIÓN DE ORACLE XE.sql
@@ -1,2 +1,8 @@
---COMANDO PARA ELIMINAR TODAS LAS TABLAS DE UNA CONEXIÓN DE ORACLE XE
-BEGIN FOR c IN (SELECT table_name FROM user_tables) LOOP EXECUTE IMMEDIATE ('DROP TABLE "' || c.table_name || '" CASCADE constraints'); END LOOP; FOR s IN (SELECT sequence_name FROM user_sequences) LOOP EXECUTE IMMEDIATE ('DROP SEQUENCE ' || s.sequence_name); END LOOP; END;
+--COMANDO PARA ELIMINAR TODAS LAS TABLAS DE UNA CONEXIÃ“N DE ORACLE XE
+BEGIN FOR c IN (SELECT table_name FROM user_tables) 
+LOOP EXECUTE IMMEDIATE ('DROP TABLE "' || c.table_name || '" CASCADE constraints'); 
+END LOOP; 
+FOR s IN (SELECT sequence_name FROM user_sequences) 
+LOOP EXECUTE IMMEDIATE ('DROP SEQUENCE ' || s.sequence_name); 
+END LOOP; 
+END;
